4d8e878ee984701e13adb8c6665a67d74380c109
KovidaOpatha <kovidaopathaz@gmail.com>
2024-03-19 22:50:09 +0530

    Initial Commit

--- src/PlaneManagement.java
+++ src/PlaneManagement.java
@@ -148,6 +148,7 @@ public class PlaneManagement {
     private static void find_first_available(Scanner scanner) {
         boolean found = false;
 
+
         for (int i = 0; i < seats.length; i++) {
             for (int j = 0; j < seats[i].length; j++) {
                 if (seats[i][j].equals("0")) {
